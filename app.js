7c3f35d145592bbdd97b4f300ff81457ef5559a4
alticator <main@alticator.com>
2021-01-04 19:50:58 +0200

    Initialize and make a simple welcome message

---
+++ app.js
@@ -0,0 +1,6 @@
+// Alticator Editor
+var app = "Alticator Editor";
+function startProgram(appName) {
+    return "Welcome to " + appName;
+}
+document.body.innerHTML = startProgram(app);
---
+++ app.ts
@@ -0,0 +1,9 @@
+// Alticator Editor
+
+let app: string = "Alticator Editor";
+
+function startProgram(appName: string) {
+	return `Welcome to ${appName}`;
+}
+
+document.body.innerHTML = startProgram(app);
